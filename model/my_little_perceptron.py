bbed6eb0477343916390e25c0e2e2a592e907487
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2023-12-15 01:33:26 -0600

    Prune unused attention parameters

--- model/my_little_perceptron.py
+++ model/my_little_perceptron.py
@@ -317,10 +317,7 @@ class BasicLayer(nn.Module):
         num_heads,
         window_size,
         mlp_ratio=4.0,
-        qkv_bias=True,
-        qk_scale=None,
         drop=0.0,
-        attn_drop=0.0,
         drop_path=0.0,
         norm_layer=nn.LayerNorm,
         downsample=None,
@@ -633,10 +630,7 @@ class QwinSR(nn.Module):
         num_heads (tuple(int)): Number of attention heads in different layers.
         window_size (int): Window size. Default: 7
         mlp_ratio (float): Ratio of mlp hidden dim to embedding dim. Default: 4
-        qkv_bias (bool): If True, add a learnable bias to query, key, value. Default: True
-        qk_scale (float): Override default qk scale of head_dim ** -0.5 if set. Default: None
         drop_rate (float): Dropout rate. Default: 0
-        attn_drop_rate (float): Attention dropout rate. Default: 0
         drop_path_rate (float): Stochastic depth rate. Default: 0.1
         norm_layer (nn.Module): Normalization layer. Default: nn.LayerNorm.
         ape (bool): If True, add absolute position embedding to the patch embedding. Default: False
